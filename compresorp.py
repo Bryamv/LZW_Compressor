52502d38802fcf67c81456e9385ab115033c50be
Bryam Anderson Villalobos De Leon <bryamv@uninorte.edu.co>
2023-05-13 10:43:53 -0500

    malo

--- compresorp.py
+++ compresorp.py
@@ -3,13 +3,12 @@ import sys
 from mpi4py import MPI
 import os
 import math
-dictionary = {}
-for i in range(256):
-        dictionary[bytes([i])] = i
-def lzw_compress(data):
-    
-    
 
+
+def lzw_compress(data, max_dict_size=2**16):
+    dictionary = {}
+    for i in range(256):
+        dictionary[bytes([i])] = i.to_bytes(4, byteorder='big')
     result = []
     buffer = b""
     for byte in data:
@@ -17,13 +16,15 @@ def lzw_compress(data):
         if new_buffer in dictionary:
             buffer = new_buffer
         else:
-            result.append(dictionary[buffer])
-            dictionary[new_buffer] = len(dictionary)
+            result.append(int.from_bytes(dictionary[buffer], byteorder='big'))
+            dictionary[new_buffer] = len(dictionary).to_bytes(2, byteorder='big')
             buffer = bytes([byte])
+            if len(dictionary) >= max_dict_size:
+                break
     if buffer:
-        result.append(dictionary[buffer])
+        result.append(int.from_bytes(dictionary[buffer], byteorder='big'))
 
-    return result
+    return result, dictionary
 
 
 def compress_file(input_file_path, output_file_path):
@@ -32,51 +33,51 @@ def compress_file(input_file_path, output_file_path):
     size = comm.Get_size()
 
     file_size = os.path.getsize(input_file_path)
-
-    # Calcula el tamaño de cada parte en bytes
     part_size = math.ceil(file_size / size)
 
     file_parts = []
     with open(input_file_path, 'rb') as file:
         for i in range(size):
-            # Lee n bytes del archivo
             data = file.read(part_size)
-            # Agrega los n bytes a la lista de fragmentos
             file_parts.append(data)
 
-
-    # Distribuir la lista entre los procesos
     data = comm.scatter(file_parts, root=0)
-    
-    
-    if rank == 0:
-        compressed_text = lzw_compress(data)
 
-        data = compressed_text
+    if rank == 0:
+        compressed_text, dictionary = lzw_compress(data)
+        compressed_data = bytearray()
+        for code in compressed_text:
+            compressed_data.extend(code.to_bytes(4, byteorder="big"))
 
         for i in range(1, size):
-            # Recibe los datos de cada proceso
-            data += comm.recv(source=i)
-            
-            # Escribe los datos en el archivo de salida
-            with open(output_file_path, "wb") as output_file:
-                for code in data:
-                    output_file.write(code.to_bytes(4, byteorder="big"))
-    else: 
-        # Comprime los datos recibidos
-        compressed_text = lzw_compress(data)
-        # Convierte los datos comprimidos a bytes
-        # Envía los datos comprimidos al proceso 0
-        print(f"proceso {rank}: {(compressed_text)}")
-        comm.send(compressed_text, dest=0)
+            comm.send(dictionary, dest=i)
+
+        final_data = compressed_data
+
+        for i in range(1, size):
+            received_data = comm.recv(source=i)
+            final_data.extend(received_data)
+
+        with open(output_file_path, "wb") as output_file:
+            output_file.write(final_data)
+
+    else:
+        dictionary = comm.bcast(None, root=0)
+        compressed_text, _ = lzw_compress(data, max_dict_size=len(dictionary))
+
+        compressed_data = bytearray()
+        for code in compressed_text:
+            compressed_data.extend(code.to_bytes(4, byteorder="big"))
+
+        comm.send(compressed_data, dest=0)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Debes especificar el nombre del archivo a comprimir")
+    if len(sys.argv) != 3:
+        print("Debes especificar el nombre del archivo a comprimir y el nombre del archivo de salida")
         sys.exit()
     input_file_path = sys.argv[1]
-    output_file_path = "comprimido.elmejorprofesor"
+    output_file_path = sys.argv[2]
 
     start_time = time.time()
     compress_file(input_file_path, output_file_path)
