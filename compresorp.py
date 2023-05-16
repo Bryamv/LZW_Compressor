77db9a0e8e82390c7bf9ccbc001b77635291bba1
Andres Felipe Vargas Vasquez <AndresFVargas10@outlook.com>
2023-05-16 10:55:06 -0500

    Trabajo grupal -Ehider - Bryam - Andres

--- compresorp.py
+++ compresorp.py
@@ -6,6 +6,11 @@ import math
 dictionary = {}
 for i in range(256):
         dictionary[bytes([i])] = i
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
 def lzw_compress(data):
     
     
@@ -27,9 +32,6 @@ def lzw_compress(data):
 
 
 def compress_file(input_file_path, output_file_path):
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
 
     file_size = os.path.getsize(input_file_path)
 
@@ -57,14 +59,27 @@ def compress_file(input_file_path, output_file_path):
         with open(f'comprimido_{rank}.bin', "wb") as output_file:
                 for code in data:
                     output_file.write(code.to_bytes(4, byteorder="big"))
-        #for i in range(1, size):
-            # Recibe los datos de cada proceso
-            #data += comm.recv(source=i)
-            #
-            # Escribe los datos en el archivo de salida
-            #with open(output_file_path, "wb") as output_file:
-             #   for code in data:
-               #     output_file.write(code.to_bytes(4, byteorder="big"))
+        
+        comm.barrier()
+
+        if os.path.exists(output_file_path):
+            os.remove(output_file_path)
+        for i in range(size):
+            
+            with open(f'comprimido_{rank}.bin', "rb") as input_file, open(output_file_path, "ab") as output_file:
+                while True:
+                    # Lee 4 bytes del archivo
+                    code = input_file.read(4)
+                    if not code:
+                        break
+                    # Convierte los 4 bytes a un número entero
+                    code = int.from_bytes(code, byteorder="big")
+                    # Convierte el número entero a 4 bytes
+                    output_file.write(code.to_bytes(4, byteorder="big"))
+
+        for i in range(size):
+            if os.path.exists(f'comprimido_{i}.bin'):
+                    os.remove(f'comprimido_{i}.bin')
     else: 
         # Comprime los datos recibidos
         compressed_text = lzw_compress(data)
@@ -76,6 +91,9 @@ def compress_file(input_file_path, output_file_path):
                 for code in compressed_text:
                     output_file.write(code.to_bytes(4, byteorder="big"))
 
+        comm.barrier()
+
+   
         #comm.send(compressed_text, dest=0)
 
 
@@ -89,5 +107,5 @@ if __name__ == "__main__":
     start_time = time.time()
     compress_file(input_file_path, output_file_path)
     end_time = time.time()
-
     print(f"El tiempo de ejecución fue: {end_time - start_time:.2f} segundos")
+
